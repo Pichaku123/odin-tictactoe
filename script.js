d9cab5e39fe8c9d003c10e84cb1eeb49df343f75
Pichaku123 <pribisht785@gmail.com>
2025-07-04 15:22:01 +0530

    gonna try rethinking it all, just saving it rn

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 function GameBoard(){
     const board=[];
     const n=3;
+    
+    const getBoard= () => {     //uses closure to print the board
+        console.log(board);
+        return board;
+    }; 
+
     for(let i=0; i<n; i++)
     {
         board[i]=[];
         for(let j=0; j<n; j++)
         {
-            board[i].push("x");
+            board[i].push(Cell());
         }
     }
     
@@ -16,7 +22,50 @@ function GameBoard(){
             console.log(cell);
         })
     };
-    return {board, printBoard};
+
+    const playMove=() => {
+        
+    }
+
+    return {getBoard, printBoard, playMove};
 } 
+
+//cell can have 3 values, 0 mean empty, 1 means p1, 2 means p2.
+//it has properties like player, move
+function Cell(){
+    const player=0;
+    const move="";
+    return {player, move};
+}
+
+
+function GamePlay(){
+    const board=GameBoard();
+    const p1="player 1";
+    const p2="player 2";
+
+    const players=[
+        {
+            name : playerOne,
+            player : 1,
+        },
+        {
+            name: playerTwo,
+            player: 2,
+        }
+    ]
+
+    let currentPlayer=players[0];
+
+    const swapPlayer= () =>{
+        currentPlayer=(currentPlayer === players[0]) ?
+        (players[1]) : (players[0]);
+    }
+
+    
+    
+    
+} 
+
 const play=GameBoard();
-console.log(play.board)
+play.getBoard();
